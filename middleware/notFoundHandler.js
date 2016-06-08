c1eb888fd861c68b9609314bf7224589a107a279
jodid93 <jodid93@gmial.com>
2016-06-08 22:54:03 +0000

    eg reyni og reyni og reyni en ekkert cirdist ganga upp

--- middleware/notFoundHandler.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = function notFoundHandler(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-};
--- webBackend/middleware/errorHandler.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function errorHandler (err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: err
-  });
-};
